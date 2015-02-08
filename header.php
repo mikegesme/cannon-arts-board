54800db4615fc064a78af64788e82ee790e0d1c3
Mike Gesme <mikegesme@users.noreply.github.com>
2015-02-08 11:14:28 -0600

    Browser support for responsive image banner.

--- header.php
+++ header.php
@@ -1,11 +1,29 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width">
 	<title><?php the_title(); ?></title>
+	<script>
+	// Picture element HTML5 shiv
+	document.createElement( "picture" );
+	</script>
+	<script src="<?php bloginfo('template_url'); ?>/js/picturefill.js" async></script>
 	<?php wp_head(); ?>
 </head>
 <header>
 	<div class="banner">
-		<img src="<?php bloginfo('template_url'); ?>/images/logo-2000.jpg" srcset="<?php bloginfo('template_url'); ?>/images/logo-1400.jpg 700w" alt="Cannon Arts Board">
+		<picture>
+			<!--[if IE 9]><video style="display: none;"><![endif]-->
+			<source srcset="<?php bloginfo('template_url'); ?>/images/logo-2000.jpg" media="(min-width: 700px)">
+			<!--[if IE 9]></video><![endif]-->
+			<!--[if lt IE 9]>
+			<img src="<?php bloginfo('template_url'); ?>/images/logo-2000.jpg" alt="Cannon Arts Board">
+			<![endif]-->
+			<!--[if !lt IE 9]><!-->
+			<img srcset="<?php bloginfo('template_url'); ?>/images/logo-1400.jpg" alt="Cannon Arts Board">
+			<!-- <![endif]-->
+		</picture>
 	</div>
+</header>
 <body>
