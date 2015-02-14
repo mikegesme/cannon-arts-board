ecd3835bd34a4e1f24dd121044bcd67e0558fe80
Mike Gesme <mikegesme@users.noreply.github.com>
2015-02-14 13:48:45 -0600

    Styling for events and sidebar.

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php get_header(); ?>
 
+<div class="main-content clearfix">
 <div class="two-thirds">
 
 	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
@@ -8,6 +9,11 @@
 
 	<?php endwhile; endif; ?>
 
+</div>
+
+<div class="sidebar one-third">
+	<?php dynamic_sidebar('primary'); ?>
+</div>
 </div>
 
 <?php get_footer(); ?>
