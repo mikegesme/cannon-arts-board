18426ce4a9e5d5b634b20bd8366a735c77c07784
Mike Gesme <mikegesme@users.noreply.github.com>
2015-02-16 14:41:24 -0600

    Added newsletter signup.

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
 			<div class="site-footer-wrapper">
 				<?php wp_footer(); ?>
 			</div>
-			<p class="site-copyright">Copyright &copy; <?php echo date('Y'); ?> Cannon Arts Board. Website by <a href="http://mikegesmeweb.com">Mike Gesme</a></p>
+			<p class="site-copyright">Copyright &copy; <?php echo date('Y'); ?> Cannon Arts Board.</p>
 		</footer>
 	</div> <!-- end page-wrap -->
 </body>
